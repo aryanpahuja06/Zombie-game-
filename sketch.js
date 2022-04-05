2c5e515a0251bd41df660c2ec5558cf683bb56c0
Aryan Pahuja <81355135+aryanpahuja06@users.noreply.github.com>
2022-04-05 14:39:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var zombie;
+
+
+
+function setup()
+{
+  createCanvas(windowWidth,windowHeight);
+zombie= createSprite(50,100,20,50)
+}
+
+function draw() 
+{
+  background(51);
+  console.log(mouseX + "," + mouseY)
+  
+ 
+}
+
